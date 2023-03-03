e40a24c8be768fd3ac12a5a26c7f08bf8e3bee8c
El-Saka <99428793+El-Saka@users.noreply.github.com>
2023-03-03 17:56:08 -0300

    Add files via upload

---
+++ Ejercicio1 Modulos ejecutando modulos como scripts/Uso-Calculadora.py
@@ -0,0 +1,11 @@
+import Calculadora
+
+resultado_suma = Calculadora.sumar(1,2)
+resultado_resta = Calculadora.restar(1,2)
+resultado_multiplicacion = Calculadora.multiplicar(1,2)
+resultado_division = Calculadora.dividir(1,2)
+
+print("El resultado de la suma es: ", resultado_suma)
+print("El resultado de la resta es: ", resultado_resta)
+print("El resultado de la multiplicacion es: ", resultado_multiplicacion)
+print("El resultado de la division es: ", resultado_division)
